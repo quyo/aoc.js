01ce62bb7cefee5de64be4622dbb247969ec3c26
Markus John <johm@quyo.de>
2022-12-11 20:03:29 +0100

    Upgrade to new template version

--- src/frontend/public/js/app.ts
+++ src/frontend/public/js/app.ts
@@ -1,1 +1,21 @@
+// Bootstrap demo
+
+import { Popover } from "bootstrap";
+
+new Popover(document.getElementById("popoverButton") ?? "");
+
+// jQuery demo
+
+import jQuery from "jquery";
+/* eslint-disable */
+(<any>window).$ = jQuery;
+(<any>window).jQuery = jQuery;
+/* eslint-enable */
+
+jQuery(function () {
+  $("p").text("The DOM is now loaded and can be manipulated.");
+});
+
+// NODE_ENV demo
+
 console.log(process.env["NODE_ENV"]);
